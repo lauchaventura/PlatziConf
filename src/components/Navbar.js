707be8cac222be5bc79e64aa9355fd0061a60e20
lauchaventura <lautaroventura02@gmail.com>
2021-08-04 22:10:20 -0300

    start

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles/navbar.css';
+import logo from '../images/logo.svg';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <div className="Navbar">
+                <div className="container-fluid">
+                    <Link className="Navbar__brand" to="/">
+                        <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+                        <span className="fw-light">Platzi</span>
+                        <span className="fw-bold">Conf</span>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
